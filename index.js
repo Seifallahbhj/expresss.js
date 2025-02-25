0b3fafe1d8c3f7a220c1e32eb4a98d1d846d695c
Seifallahbhj <seifallah.bhj@gmail.com>
2025-02-25 20:05:06 +0100

    add time fuction

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const __dirname=path.resolve()
 const app=express()
 dotenv.config()
 const PORT=process.env.PORT ||4000
+app.use((req,res,next)=>{
+    let currentDate=new Date()
+    const day=currentDate.getDay()
+    const hour=currentDate.getHours()
+    if (day>0 && day<6 && hour>9 && hour<21){
+        next()
+    }else res.send("thank you for visiting")
+})
 app.use('/api',router)
 app.use((req,res,next)=>{res.sendFile(path.join(__dirname,'page','404.html'))})
 app.listen(PORT,()=>{console.log('server is runing on port',PORT)})
